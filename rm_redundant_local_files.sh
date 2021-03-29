2980df1be5752de96533f7ad5967f93fb2adc2bd
Jai <j.bhagat@ucl.ac.uk>
2021-03-29 01:32:52 +0100

    ensure runs by bash, not just by dash or some other shell

--- rm_redundant_local_files.sh
+++ rm_redundant_local_files.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 # Check ceph server for files, and if found, delete them from local.
 
 # <s User modifiable constants
 LOCAL_PATH='/mnt/c/ProjectAeon/'
-REMOTE_PATH='/mnt/z/'  # \\ceph-gw01.hpc.swc.ucl.ac.uk\aeon\test2\
+#REMOTE_PATH='/mnt/z/'  # \\ceph-gw01.hpc.swc.ucl.ac.uk\aeon\test2\
+REMOTE_PATH='/mnt/c/ProjectAeon/remote/'
 EXP_NAME='experiment0'
 # hardware devices as top-level folders
 DEVICES=('camera_side' 'camera_top' 'microphone' 'harp_patch1' 'harp_patch2' \
@@ -23,15 +26,15 @@ test ! -d ${REMOTE_EXP_DIR} \
 && echo "Remote exp dir ${REMOTE_EXP_DIR} DOES NOT exist." && exit 1
 
 # Remove files, device-by-device.
-for device in ${DEVICES[@]}; do
-	# files_local=($(find "${LOCAL_EXP_DIR}${device}/" -type f -printf "%f\n"))
+for device in "${DEVICES[@]}"; do
+	#files_local=($(find "${LOCAL_EXP_DIR}${device}/" -type f -printf "%f\n"))
 	files_local=($(find "${LOCAL_EXP_DIR}${device}/" -type f))
 	files_remote=($(find "${REMOTE_EXP_DIR}${device}/" -type f))
 	# todo: could try to use associative arrays (or something else) instead of
 	# nested loop
-	for fl in ${files_local[@]}; do
+	for fl in "${files_local[@]}"; do
 		match_found=0
-		for fr in ${files_remote[@]}; do
+		for fr in "${files_remote[@]}"; do
 			if [ $(basename ${fl}) == $(basename ${fr}) ]; then 
 				rm ${fl}
 				match_found=1
--- transfer_chunked_files.sh
+++ transfer_chunked_files.sh
@@ -1,16 +1,20 @@
+#!/bin/bash
+
 # Given an experiment name and devices, create folders on acquisition computer
 # and mounted ceph server. While experiment is running, check for new files; if 
 # found, copy over from acquisition computer to ceph server.
 
 # <s User modifiable constants
 LOCAL_PATH='/mnt/c/ProjectAeon/'
-REMOTE_PATH='/mnt/z/'  # \\ceph-gw01.hpc.swc.ucl.ac.uk\aeon\test2\
+#REMOTE_PATH='/mnt/z/'  # \\ceph-gw01.hpc.swc.ucl.ac.uk\aeon\test2\
+REMOTE_PATH='/mnt/c/ProjectAeon/remote/'
 EXP_NAME='experiment0'
 # hardware devices as top-level folders
 DEVICES=('camera_side' 'camera_top' 'microphone' 'harp_patch1' 'harp_patch2' \
          'harp_patch3')
 # time of files in seconds
-CHUNK_TIME=3600
+#CHUNK_TIME=3600
+CHUNK_TIME=1
 # /s>
 
 # <s Script constants (do not edit!)
@@ -36,24 +40,27 @@ mkdir ${LOCAL_EXP_DIR}
 cd ${LOCAL_EXP_DIR}
 mkdir ${DEVICES[*]}
 
-# for device in ${DEVICES[@]}; do
-# 		mkdir("${LOCAL_EXP_DIR}/${device}/")
-# 		mkdir("${REMOTE_EXP_DIR}/${device}/")
-# done
+#for device in ${DEVICES[@]}; do
+    #mkdir("${LOCAL_EXP_DIR}/${device}/")
+    #mkdir("${REMOTE_EXP_DIR}/${device}/")
+#done
 # /s>
 
 # <s Check for and copy written out files
 while [[ EXP_ON ]]; do
-	for device in ${DEVICES[@]}; do
+	for device in "${DEVICES[@]}"; do
 		cur_time_s=$(date +"%s")  # seconds since 1970-01-01
-		end_time_s=$(( ${cur_time} - ${CHUNK_TIME} ))
+		end_time_s=$(( ${cur_time_s} - ${CHUNK_TIME} ))
 		end_time_fmt=$(date -d @${end_time_s} +"%c")  # formatted for `find`
-		# start_time=$(( ${end_time}-${CHUNK_TIME} ))
-		files=$(find ./${device} ! -newermt "${end_time_fmt}")
-		# files = find -newermt "${start_time}" ! -newermt "${end_times}"
-		cp -n ${files[*]} "${REMOTE_EXP_DIR}/${device}/"
+		#start_time=$(( ${end_time}-${CHUNK_TIME} ))
+		#files = find -newermt "${start_time}" ! -newermt "${end_times}"
+		files=$(find ${device} ! -newermt "${end_time_fmt}" -type f)
+		# Ensure files found before attempting to copy.
+		if (( ${#files[@]} )); then
+			cp -n ${files[*]} "${REMOTE_EXP_DIR}${device}/"
+		fi
 	done
 	# Exit when user quits Bonsai or stops workflow
-	# if [ bonsai_exits ]; EXP_ON=0; fi
+	#if [ bonsai_exits ]; EXP_ON=0; fi
 done
 # /s>
