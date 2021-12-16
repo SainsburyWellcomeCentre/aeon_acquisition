d82fc4e4f2c4152f137a081bb6734213c80fbf89
glopesdev <glopesdev@users.noreply.github.com>
2021-12-16 20:00:46 +0000

    Refactor acquisition workflow to export metadata

---
+++ workflows/Extensions/ThrowException.cs
@@ -0,0 +1,19 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+[Combinator]
+[Description("Raises an exception when the source sequence produces a value.")]
+[WorkflowElementCategory(ElementCategory.Combinator)]
+public class ThrowException
+{
+    [Description("The error message describing the exception.")]
+    public string Message { get; set; }
+
+    public IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+    {
+        return source.SelectMany(value => Observable.Throw<TSource>(new InvalidOperationException(Message)));
+    }
+}
