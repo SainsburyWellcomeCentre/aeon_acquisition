4d32d38a042236f206e267c38eed972dba49179b
glopesdev <glopesdev@users.noreply.github.com>
2022-01-19 11:58:47 +0000

    Add test project

---
+++ src/Aeon.Acquisition.Tests/OperatorTests.cs
@@ -0,0 +1,33 @@
+using Bonsai;
+using Bonsai.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Xml;
+
+namespace Aeon.Acquisition.Tests
+{
+    [TestClass]
+    public class OperatorTests
+    {
+        [TestMethod]
+        public void Build_Workflows()
+        {
+            var assembly = typeof(CreateTimestamped).Assembly;
+            foreach (var name in assembly.GetManifestResourceNames())
+            {
+                using (var workflowStream = assembly.GetManifestResourceStream(name))
+                using (var reader = XmlReader.Create(workflowStream))
+                {
+                    reader.MoveToContent();
+                    var workflowBuilder = (WorkflowBuilder)WorkflowBuilder.Serializer.Deserialize(reader);
+                    workflowBuilder.Workflow.Convert(builder =>
+                    {
+                        var workflowElement = ExpressionBuilder.GetWorkflowElement(builder);
+                        Assert.IsNotInstanceOfType(workflowElement, typeof(UnknownTypeBuilder));
+                        return builder;
+                    }, recurse: true);
+                }
+            }
+        }
+    }
+}
