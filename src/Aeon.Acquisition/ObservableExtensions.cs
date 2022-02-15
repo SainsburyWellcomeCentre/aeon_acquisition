89fc56014efd134b17517912e20ece3ffdea6071
glopesdev <glopesdev@users.noreply.github.com>
2022-02-15 15:04:05 +0000

    Add support for tracking estimated dispenser state

---
+++ src/Aeon.Acquisition/ObservableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Aeon.Acquisition
+{
+    static class ObservableExtensions
+    {
+        public static IObservable<Unit> MergeUnit<TSource, TOther>(this IObservable<TSource> source, IObservable<TOther> other)
+        {
+            return source.Select(x => Unit.Default).Merge(other.Select(x => Unit.Default));
+        }
+    }
+}
---
+++ src/Aeon.Acquisition/StateRecovery.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Aeon.Acquisition
+{
+    public static class StateRecovery<TState> where TState : new()
+    {
+        static string GetFileName(string name) => $"~{typeof(TState).Name}.{name}.tmp";
+
+        public static void Serialize(string name, TState value)
+        {
+            var fileName = GetFileName(name);
+            var serializer = new XmlSerializer(typeof(TState));
+            using (var writer = XmlWriter.Create(fileName, new XmlWriterSettings { Indent = true }))
+            {
+                serializer.Serialize(writer, value);
+            }
+        }
+
+        public static TState Deserialize(string name)
+        {
+            var fileName = GetFileName(name);
+            if (!File.Exists(fileName))
+            {
+                return new TState();
+            }
+
+            var serializer = new XmlSerializer(typeof(TState));
+            using (var reader = XmlReader.Create(fileName))
+            {
+                return (TState)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
