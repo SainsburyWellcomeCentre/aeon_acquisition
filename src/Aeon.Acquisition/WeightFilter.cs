a399fe5f041cbba54dfa4684639f849b1ab2ece0
glopesdev <glopesdev@users.noreply.github.com>
2022-02-16 19:49:25 +0000

    Update filter to work with weight measurement type

--- src/Aeon.Acquisition/WeightFilter.cs
+++ src/Aeon.Acquisition/WeightFilter.cs
@@ -1,42 +1,58 @@
 using Bonsai;
 using System;
 using System.ComponentModel;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
-using OpenCV.Net;
 using MathNet.Numerics;
 
 namespace Aeon.Acquisition
 {
     [Combinator]
-    [Description("Calculates the Linear regresison assuming x as sample index, return is intercept point")]
+    [Description("Calculates a linear regression filter over a sliding window of weights.")]
     [WorkflowElementCategory(ElementCategory.Transform)]
     public class WeightFilter
     {
+        [Description("The number of samples in the sliding window.")]
         public int Count { get; set; } = 10;
-        public double Threshold { get; set; }
-        public IObservable<double> Process(IObservable<double> source)
-        {
-            return source.Buffer<double>(Count, 1).Where(buf => buf.Count == Count).Select(value =>
-             {
 
-                 int samples = value.Count;
-                 double[] xData = new double[samples];
-                 for (int count = 0; count < samples; count++)
-                 {
-                     xData[count] = count;
-                 }
-                 return Fit.Line(xData, value.ToArray());
-             }).Where(f => Math.Abs(f.Item2) <= Threshold).Select(f => f.Item1);
+        public IObservable<WeightMeasurement> Process(IObservable<WeightMeasurement> source)
+        {
+            return source
+                .Buffer(Count, 1)
+                .Where(buffer => buffer.Count == Count)
+                .Select(buffer =>
+                {
+                    var xData = new double[buffer.Count];
+                    var yData = new double[buffer.Count];
+                    for (int i = 0; i < xData.Length; i++)
+                    {
+                        xData[i] = i;
+                        yData[i] = buffer[i].Value;
+                    }
+
+                    WeightMeasurement result;
+                    result.Timestamp = buffer[buffer.Count - 1].Timestamp;
+                    var (intercept, slope) = Fit.Line(xData, yData);
+                    result.Value = (float)intercept;
+                    result.Confidence = 1 - (float)(Math.Atan(slope) / Constants.PiOver2);
+                    return result;
+                });
         }
 
-        public IObservable<double> Process<TOther>(IObservable<double> source, IObservable<TOther> trigger)
+        public IObservable<WeightMeasurement> Process<TOther>(IObservable<WeightMeasurement> source, IObservable<TOther> trigger)
         {
-            return Process(source).Window(trigger).SelectMany(window => window.Publish(f => f.Take(1).CombineLatest(f, (reference, value) =>
-            {
-                return value - reference;
-            })));
+            return Process(source)
+                .Window(trigger)
+                .SelectMany(window =>
+                    window.Publish(pwindow =>
+                        pwindow.Take(1).CombineLatest(pwindow, (reference, measurement) =>
+                        {
+                            WeightMeasurement result;
+                            result.Timestamp = measurement.Timestamp;
+                            result.Confidence = measurement.Confidence;
+                            result.Value = measurement.Value - reference.Value;
+                            return result;
+                        })));
         }
     }
 }
