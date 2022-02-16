57c8194ad595ea3a54a6bc5420d86b919afdaa58
glopesdev <glopesdev@users.noreply.github.com>
2022-02-16 11:58:14 +0000

    Add support for tracking environment maintenance

---
+++ src/Aeon.Acquisition/EnvironmentStateMetadata.cs
@@ -0,0 +1,21 @@
+﻿namespace Aeon.Acquisition
+{
+    public class EnvironmentStateMetadata
+    {
+        public EnvironmentStateMetadata(string name, EnvironmentStateType state)
+        {
+            Name = name;
+            Type = state;
+        }
+
+        public string Name { get; }
+
+        public EnvironmentStateType Type { get; }
+    }
+
+    public enum EnvironmentStateType
+    {
+        Experiment,
+        Maintenance
+    }
+}
