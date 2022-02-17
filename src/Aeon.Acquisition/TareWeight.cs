5f43ecce135b5dbae0b40eecbdf4efb7519e49c7
glopesdev <glopesdev@users.noreply.github.com>
2022-02-17 18:20:37 +0000

    Add support to send tare commands to weight scale

---
+++ src/Aeon.Acquisition/TareWeight.cs
@@ -0,0 +1,12 @@
+﻿using Bonsai;
+using System.ComponentModel;
+using System.Reactive;
+
+namespace Aeon.Acquisition
+{
+    [TypeVisualizer(typeof(TareWeightVisualizer))]
+    [Description("Generates a sequence of commands to tare a weight scale.")]
+    public class TareWeight : MetadataSource<Unit>
+    {
+    }
+}
