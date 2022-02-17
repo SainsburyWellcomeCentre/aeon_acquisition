837db084add2fd01195050b1e203f23618b047b5
glopesdev <glopesdev@users.noreply.github.com>
2022-02-17 12:55:18 +0000

    Add visualizer for exposed experiment properties

---
+++ src/Aeon.Acquisition/ExperimentProperties.cs
@@ -0,0 +1,10 @@
+﻿using Bonsai;
+using Bonsai.Expressions;
+
+namespace Aeon.Acquisition
+{
+    [TypeVisualizer(typeof(ExternalizedPropertiesVisualizer))]
+    public class ExperimentProperties : UnitBuilder
+    {
+    }
+}
---
+++ src/Aeon.Acquisition/ExperimentPropertiesVisualizer.cs
@@ -0,0 +1,39 @@
+﻿using Bonsai;
+using Bonsai.Design;
+using System;
+
+namespace Aeon.Acquisition
+{
+    sealed class ExternalizedPropertiesVisualizer : DialogTypeVisualizer
+    {
+        PropertyGrid control;
+
+        public override void Load(IServiceProvider provider)
+        {
+            var workflowBuilder = (WorkflowBuilder)provider.GetService(typeof(WorkflowBuilder));
+
+            control = new PropertyGrid();
+            control.Dock = System.Windows.Forms.DockStyle.Fill;
+            control.SelectedObject = workflowBuilder.Workflow;
+
+            var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
+            if (visualizerService != null)
+            {
+                visualizerService.AddControl(control);
+            }
+        }
+
+        public override void Show(object value)
+        {
+        }
+
+        public override void Unload()
+        {
+            if (control != null)
+            {
+                control.Dispose();
+                control = null;
+            }
+        }
+    }
+}
