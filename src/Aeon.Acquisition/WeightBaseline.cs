2769b7861b62a8506ccfff0453f7224757c54ae2
glopesdev <glopesdev@users.noreply.github.com>
2022-02-18 10:26:32 +0000

    Avoid reporting weights before the first baseline

--- src/Aeon.Acquisition/WeightBaseline.cs
+++ src/Aeon.Acquisition/WeightBaseline.cs
@@ -14,17 +14,15 @@ namespace Aeon.Acquisition
         public IObservable<WeightMeasurement> Process<TOther>(IObservable<WeightMeasurement> source, IObservable<TOther> trigger)
         {
             return source
-                .Window(trigger)
-                .SelectMany((window, index) =>
+                .Window(trigger).Skip(1)
+                .SelectMany(window =>
                     window.Publish(pwindow =>
                         pwindow.Take(1).CombineLatest(pwindow, (reference, measurement) =>
                         {
                             WeightMeasurement result;
                             result.Timestamp = measurement.Timestamp;
                             result.Confidence = measurement.Confidence;
-                            result.Value = index > 0
-                                ? measurement.Value - reference.Value
-                                : measurement.Value;
+                            result.Value = measurement.Value - reference.Value;
                             return result;
                         })));
         }
