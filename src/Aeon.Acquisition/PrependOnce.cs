146df48d35f2c9b75935ce918884807a9a3b867f
glopesdev <glopesdev@users.noreply.github.com>
2022-02-21 22:53:11 +0000

    Add operator to support init-only state recovery

---
+++ src/Aeon.Acquisition/PrependOnce.cs
@@ -0,0 +1,29 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+
+namespace Aeon.Acquisition
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Combinator)]
+    [Description("Subscribes only once to the auxiliary sequence and returns the stored value before publishing the main sequence.")]
+    public class PrependOnce
+    {
+        public IObservable<TSource> Process<TSource>(IObservable<TSource> source, IObservable<TSource> other)
+        {
+            var first = true;
+            return Observable.Create<TSource>(async (observer, cancellationToken) =>
+            {
+                if (first)
+                {
+                    var value = await other;
+                    observer.OnNext(value);
+                    first = false;
+                }
+
+                return source.SubscribeSafe(observer);
+            });
+        }
+    }
+}
