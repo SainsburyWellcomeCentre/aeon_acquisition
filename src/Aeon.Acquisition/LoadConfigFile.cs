a0927a7d96f30e00de9c4daeb2be83d4cc3dd380
glopesdev <glopesdev@users.noreply.github.com>
2022-03-10 14:36:47 +0000

    Add operator for loading YML config dictionary

---
+++ src/Aeon.Acquisition/LoadConfigFile.cs
@@ -0,0 +1,32 @@
+﻿using Bonsai;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Reactive.Linq;
+using YamlDotNet.Serialization;
+
+namespace Aeon.Acquisition
+{
+    [Description("Constructs a key-value config dictionary from a YAML/JSON file.")]
+    public class LoadConfigFile : Source<Dictionary<string, string>>
+    {
+        [Editor(DesignTypes.OpenFileNameEditor, DesignTypes.UITypeEditor)]
+        [FileNameFilter("YAML file (*.yml)|*.yml|JSON file (*.json)|*.json|All Files|*.*")]
+        [Description("The path to the file containing the dictionary values.")]
+        public string FileName { get; set; }
+
+        public override IObservable<Dictionary<string, string>> Generate()
+        {
+            return Observable.Defer(() =>
+            {
+                var deserializer = new DeserializerBuilder().Build();
+                using (var reader = File.OpenText(FileName))
+                {
+                    var dictionary = deserializer.Deserialize<Dictionary<string, string>>(reader);
+                    return Observable.Return(dictionary);
+                }
+            });
+        }
+    }
+}
