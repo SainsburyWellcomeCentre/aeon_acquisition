0b2d61a6b8682de28ee909d1a4d789e219b396e4
glopesdev <glopesdev@users.noreply.github.com>
2022-03-10 11:11:03 +0000

    Add operator for retrieving service account name

---
+++ src/Aeon.Acquisition/ServiceAccountName.cs
@@ -0,0 +1,22 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Aeon.Acquisition
+{
+    [Description("Returns the service account name used to record data acquired by the local computer.")]
+    public class ServiceAccountName : Source<string>
+    {
+        public override IObservable<string> Generate()
+        {
+            return Observable.Return(Environment.MachineName);
+        }
+
+        public IObservable<string> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(_ => Environment.MachineName);
+        }
+    }
+}
