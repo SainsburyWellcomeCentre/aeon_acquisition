fa338686e483cdf29a42d03fdcda391e55e814ce
glopesdev <glopesdev@users.noreply.github.com>
2022-04-29 01:10:40 +0100

    Generalize position tracking to multiple objects

---
+++ src/Aeon.Acquisition/DistanceFromPoint.cs
@@ -0,0 +1,45 @@
+﻿using Bonsai;
+using Bonsai.Harp;
+using Bonsai.Vision;
+using OpenCV.Net;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Aeon.Acquisition
+{
+    [Combinator]
+    [Description("Computes the distance from every component in the sequence to a point.")]
+    public class DistanceFromPoint
+    {
+        [Description("Specifies the point used to calculate the distance from.")]
+        [Editor("Bonsai.Vision.Design.IplImageInputRoiEditor, Bonsai.Vision.Design", DesignTypes.UITypeEditor)]
+        public Point Value { get; set; }
+
+        static double Distance(Point2f left, Point2f right)
+        {
+            var delta = right - left;
+            return Math.Sqrt(delta.X * delta.X + delta.Y * delta.Y);
+        }
+
+        public IObservable<Timestamped<double>> Process(IObservable<Tuple<ConnectedComponent, double>> source)
+        {
+            return source.Select(x =>
+            {
+                var distance = Distance(x.Item1.Centroid, new Point2f(Value));
+                return Timestamped.Create(distance, x.Item2);
+            });
+        }
+
+        public IObservable<Timestamped<double>> Process(IObservable<Tuple<ConnectedComponentCollection, double>> source)
+        {
+            return source.Select(x =>
+            {
+                var point = new Point2f(Value);
+                var distance = x.Item1.Min(component => Distance(point, component.Centroid));
+                return Timestamped.Create(distance, x.Item2);
+            });
+        }
+    }
+}
--- src/Aeon.Acquisition/RegionContainsPoint.cs
+++ src/Aeon.Acquisition/RegionContainsPoint.cs
@@ -13,17 +13,41 @@ namespace Aeon.Acquisition
     [Description("Generates boolean values indicating whether each point in the sequence is inside a region of interest.")]
     public class RegionContainsPoint
     {
-        readonly ContainsPoint containsPoint = new ContainsPoint();
-
         [Description("The array of vertices specifying the region of interest.")]
         [Editor("Bonsai.Vision.Design.IplImageInputRoiEditor, Bonsai.Vision.Design", DesignTypes.UITypeEditor)]
         public Point[][] Regions { get; set; }
-        
+
+        static bool Contains(Point[][] contour, Point2f point)
+        {
+            if (contour == null) return false;
+            for (int i = 0; i < contour.Length; i++)
+            {
+                using (var contourHeader = Mat.CreateMatHeader(contour[i], contour[i].Length, 2, Depth.S32, 1))
+                {
+                    return CV.PointPolygonTest(contourHeader, point, false) > 0;
+                }
+            }
+
+            return false;
+        }
+
         public IObservable<Timestamped<bool>> Process(IObservable<Tuple<ConnectedComponent, double>> source)
         {
-            return source.Publish(ps => ps.Zip(
-                containsPoint.Process(ps.Select(x => Tuple.Create(x.Item1.Centroid, Regions))),
-                (time, result) => Timestamped.Create(result, time.Item2)));
+            return source.Select(x =>
+            {
+                var containsPoint = Contains(Regions, x.Item1.Centroid);
+                return Timestamped.Create(containsPoint, x.Item2);
+            });
+        }
+
+        public IObservable<Timestamped<bool>> Process(IObservable<Tuple<ConnectedComponentCollection, double>> source)
+        {
+            return source.Select(x =>
+            {
+                var regions = Regions;
+                var containsPoint = x.Item1.Any(component => Contains(regions, component.Centroid));
+                return Timestamped.Create(containsPoint, x.Item2);
+            });
         }
     }
 }
