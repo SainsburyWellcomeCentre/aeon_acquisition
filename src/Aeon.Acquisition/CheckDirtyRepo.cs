6cafaa7c2531df6a2556d6164e4d230891d3eef3
Bruno Cruz <bfpcruz@gmail.com>
2022-06-27 21:52:51 +0100

    Add operator to determine if a repository is dirty

---
+++ src/Aeon.Acquisition/CheckDirtyRepo.cs
@@ -0,0 +1,31 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using LibGit2Sharp;
+
+namespace Aeon.Acquisition
+{
+
+    [Combinator]
+    [Description("From a Repository object determines if the repository is dirty or uncommited changes exist")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+
+    public class CheckDirtyRepo
+    {
+
+        public IObservable<Boolean> Process(IObservable<IRepository> source)
+        {
+            return source.Select(value =>
+            {
+                RepositoryStatus status = value.RetrieveStatus();
+                bool isDirtyRepostatus = status.IsDirty;
+                bool isUncommitedFiles = value.Diff.Compare<TreeChanges>().Count > 0;
+                return isDirtyRepostatus & isUncommitedFiles;
+            });
+        }
+
+
+    }
+
+}
