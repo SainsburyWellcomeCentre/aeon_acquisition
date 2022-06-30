257220954c7906155a64b6cc23122b164d80c171
Bruno Cruz <bfpcruz@gmail.com>
2022-06-30 12:38:33 +0100

    Updated to reflect PR review

---
+++ src/Aeon.Acquisition/CreateRepository.cs
@@ -0,0 +1,27 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using LibGit2Sharp;
+
+namespace Aeon.Acquisition
+{
+    [DefaultProperty(nameof(Path))]
+    [Description("Creates a repository object from the specified working directory or .git folder.")]
+    public class CreateRepository : Source<IRepository>
+    {
+        [Description("The relative or absolute path of the repository directory.")]
+        [Editor("Bonsai.Design.FolderNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        public string Path { get; set; } = "";
+
+        public override IObservable<IRepository> Generate()
+        {
+            return Observable.Defer(() =>
+            {
+                var path = string.IsNullOrEmpty(Path) ? Environment.CurrentDirectory : Path;
+                Repository repo = new Repository(path);
+                return Observable.Return(repo);
+            });
+        }
+    }
+}
--- src/Aeon.Acquisition/IsRepositoryClean.cs
+++ src/Aeon.Acquisition/IsRepositoryClean.cs
@@ -6,14 +6,11 @@ using LibGit2Sharp;
 
 namespace Aeon.Acquisition
 {
-
     [Combinator]
-    [Description("From a Repository object determines if the repository is dirty or uncommited changes exist")]
+    [Description("Determines whether a specified repository is clean or if uncommitted or untracked changes exist")]
     [WorkflowElementCategory(ElementCategory.Transform)]
-
-    public class CheckDirtyRepo
+    public class IsRepositoryClean
     {
-
         public IObservable<Boolean> Process(IObservable<IRepository> source)
         {
             return source.Select(value =>
@@ -21,11 +18,8 @@ namespace Aeon.Acquisition
                 RepositoryStatus status = value.RetrieveStatus();
                 bool isDirtyRepostatus = status.IsDirty;
                 bool isUncommitedFiles = value.Diff.Compare<TreeChanges>().Count > 0;
-                return isDirtyRepostatus & isUncommitedFiles;
+                return !(isDirtyRepostatus | isUncommitedFiles);
             });
         }
-
-
     }
-
 }
