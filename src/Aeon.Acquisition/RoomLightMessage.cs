751cf8b4f605a45d8c2af2a50a866d6a5df7577e
glopesdev <glopesdev@users.noreply.github.com>
2022-09-20 22:26:48 +0100

    Simplify light controller for direct channel write

--- src/Aeon.Acquisition/RoomLightMessage.cs
+++ src/Aeon.Acquisition/RoomLightMessage.cs
@@ -2,22 +2,18 @@
 {
     public struct RoomLightMessage
     {
-        public string Name;
-        public int ColdWhite;
-        public int WarmWhite;
-        public int Red;
+        public int Channel;
+        public int Value;
 
-        public RoomLightMessage(string name, int coldWhite, int warmWhite, int red)
+        public RoomLightMessage(int channel, int value)
         {
-            Name = name;
-            ColdWhite = coldWhite;
-            WarmWhite = warmWhite;
-            Red = red;
+            Channel = channel;
+            Value = value;
         }
 
         public override string ToString()
         {
-            return $"RoomLight({Name}, ColdWhite:{ColdWhite}, WarmWhite:{WarmWhite}, Red:{Red})";
+            return $"RoomLight({Channel}, {Value})";
         }
     }
 }
