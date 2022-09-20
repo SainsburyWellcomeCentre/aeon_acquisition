161c294d1a6fea15d48fca6070a2823c6ccea6b2
glopesdev <glopesdev@users.noreply.github.com>
2022-09-20 12:55:50 +0100

    Update dependencies to bonsai 2.7-rc1

--- bonsai/setup.ps1
+++ bonsai/setup.ps1
@@ -1,6 +1,6 @@
 if (!(Test-Path "./Bonsai.exe")) {
     & dotnet build ../src/Aeon.sln --configuration Release
-    Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.6.3/Bonsai.zip" -OutFile "temp.zip"
+    Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.7-rc1/Bonsai.zip" -OutFile "temp.zip"
     Move-Item -Path "NuGet.config" "temp.config"
     Expand-Archive "temp.zip" -DestinationPath "." -Force
     Move-Item -Path "temp.config" "NuGet.config" -Force
