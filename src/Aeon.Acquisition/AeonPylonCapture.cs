39f87f1e25c0c2adea99624c0536e4c4a55c4d3e
glopesdev <glopesdev@users.noreply.github.com>
2022-09-21 13:44:10 +0100

    Add pylon and spinnaker specific video sources

--- src/Aeon.Acquisition/AeonPylonCapture.cs
+++ src/Aeon.Acquisition/AeonPylonCapture.cs
@@ -3,7 +3,6 @@ using System;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
-using Bonsai.Spinnaker;
 using Bonsai.Harp;
 using Bonsai.Pylon;
 
@@ -14,19 +13,8 @@ namespace Aeon.Acquisition
     {
         public AeonPylonCapture()
         {
-            ExposureTime = 1e6 / 50 - 1000;
             TriggerAddress = 68;
-            Binning = 1;
         }
-
-        [Description("The duration of each individual exposure, in microseconds. In general, this should be 1 / frameRate - 1 millisecond to prepare for next trigger.")]
-        public double ExposureTime { get; set; }
-
-        [Description("The gain of the sensor.")]
-        public double Gain { get; set; }
-
-        [Description("The size of the binning area of the sensor, e.g. a binning size of 2 specifies a 2x2 binning region.")]
-        public int Binning { get; set; }
 
         [Description("The address of the Harp register used for triggering new exposures.")]
         public int TriggerAddress { get; set; }
