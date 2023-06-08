15f949a153a9b7acf0b4282597c99f6d294d2f41
glopesdev <glopesdev@users.noreply.github.com>
2023-06-08 13:44:47 +0100

    Standardize formatting and documentation strings

--- src/Aeon.Acquisition/FormatSleapPose.cs
+++ src/Aeon.Acquisition/FormatSleapPose.cs
@@ -1,46 +1,43 @@
 ﻿using Bonsai;
 using System;
 using System.ComponentModel;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using Bonsai.Sleap;
 using Bonsai.Harp;
-using OpenCV.Net;
-using System.Drawing;
-using Bonsai.Reactive;
 
-namespace Aeon.Acquisition 
+namespace Aeon.Acquisition
 {
-        [Combinator]
-        [Description("Converts a timestamped sleap pose collection into a sequence of Harp messages. " +
-        "The output will take the form of: ArgMaxId, IdConf, N*[PartA.X, PartA.Y, PartA.Conf]")]
-        [WorkflowElementCategory(ElementCategory.Transform)]
-        public class FormatSleapPose
+    [Combinator]
+    [Description("Converts a timestamped pose collection into a sequence of Harp messages.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class FormatSleapPose
+    {
+        [Description("The address of the formatted Harp message.")]
+        public int Address { get; set; } = 255;
+
+        [Description("Optional value to override or set the identity index returned by the source pose.")]
+        public int? IdentityIndex { get; set; }
+
+        public IObservable<HarpMessage> Process(IObservable<Tuple<PoseIdentity, double>> source)
         {
-            public int Address { get; set; } = 255;
-
-            [Description("Optional value to override the ArgMax value returned by the predict Sleap operator.")]
-            public int? Id { get; set; }
-
-            public IObservable<HarpMessage> Process(IObservable<Tuple<PoseIdentity,double>> source)
+            return source.Select(value =>
             {
-                return source.Select(value => {
-                    var pose = value.Item1;
-                    var timestamp = value.Item2;
-                    var data = new float[2 + pose.Count*3];
-                    data[0] = Id == null ? (float) pose.IdentityIndex : (float) Id;
-                    data[1] = pose.Confidence;
-                    int i = 1;
-                    foreach (var bp in (pose))
-                    {
-                        data[++i] = bp.Position.X;
-                        data[++i] = bp.Position.Y;
-                        data[++i] = bp.Confidence;
-                    }
-                    return HarpMessage.FromSingle(Address, timestamp, MessageType.Event, data);
-                });
-            }
+                var pose = value.Item1;
+                var timestamp = value.Item2;
+                var data = new float[2 + pose.Count * 3];
+                data[0] = IdentityIndex == null ? pose.IdentityIndex : (float)IdentityIndex;
+                data[1] = pose.Confidence;
+                int i = 1;
+                foreach (var bp in pose)
+                {
+                    data[++i] = bp.Position.X;
+                    data[++i] = bp.Position.Y;
+                    data[++i] = bp.Confidence;
+                }
+                return HarpMessage.FromSingle(Address, timestamp, MessageType.Event, data);
+            });
+        }
 
         public IObservable<HarpMessage> Process(IObservable<Tuple<PoseIdentityCollection, double>> source)
         {
@@ -51,10 +48,10 @@ namespace Aeon.Acquisition
                 return poseCollection.Select((pose, index) =>
                 {
                     var data = new float[2 + pose.Count * 3];
-                    data[0] = Id == null ? (float)pose.IdentityIndex : (float) Id;
+                    data[0] = IdentityIndex == null ? pose.IdentityIndex : (float)IdentityIndex;
                     data[1] = pose.Confidence;
                     int i = 1;
-                    foreach (var bp in (pose))
+                    foreach (var bp in pose)
                     {
                         data[++i] = bp.Position.X;
                         data[++i] = bp.Position.Y;
@@ -66,23 +63,24 @@ namespace Aeon.Acquisition
         }
 
         public IObservable<HarpMessage> Process(IObservable<Tuple<Pose, double>> source)
+        {
+            return source.Select(value =>
             {
-                return source.Select(value => {
-                    var pose = value.Item1;
-                    var timestamp = value.Item2;
-                    var data = new float[2 + pose.Count * 3];
-                    data[0] = Id == null ? -1F : (float) Id;
-                    data[1] = float.NaN;
-                    int i = 1;
-                    foreach (var bp in (pose))
-                    {
-                        data[++i] = bp.Position.X;
-                        data[++i] = bp.Position.Y;
-                        data[++i] = bp.Confidence;
-                    }
-                    return HarpMessage.FromSingle(Address, timestamp, MessageType.Event, data);
-                });
-            }
+                var pose = value.Item1;
+                var timestamp = value.Item2;
+                var data = new float[2 + pose.Count * 3];
+                data[0] = IdentityIndex == null ? -1f : (float)IdentityIndex;
+                data[1] = float.NaN;
+                int i = 1;
+                foreach (var bp in pose)
+                {
+                    data[++i] = bp.Position.X;
+                    data[++i] = bp.Position.Y;
+                    data[++i] = bp.Confidence;
+                }
+                return HarpMessage.FromSingle(Address, timestamp, MessageType.Event, data);
+            });
+        }
 
         public IObservable<HarpMessage> Process(IObservable<Tuple<PoseCollection, double>> source)
         {
@@ -93,7 +91,7 @@ namespace Aeon.Acquisition
                 return poseCollection.Select((pose, index) =>
                 {
                     var data = new float[2 + pose.Count * 3];
-                    data[0] = Id == null ? index : (float) Id;
+                    data[0] = IdentityIndex == null ? index : (float)IdentityIndex;
                     data[1] = float.NaN;
                     int i = 1;
                     foreach (var bp in pose)
