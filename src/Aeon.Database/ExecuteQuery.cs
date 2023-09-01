3fd0c96431261ab75bdb165a2fcddb718ff96d60
glopesdev <glopesdev@users.noreply.github.com>
2023-09-01 22:58:37 +0100

    Add database package core operators

---
+++ src/Aeon.Database/ExecuteQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Bonsai;
+using MySqlConnector;
+
+namespace Aeon.Database
+{
+    [DefaultProperty(nameof(QueryString))]
+    [Description("Runs the specified SQL query against each MySQL connection in the sequence.")]
+    public class ExecuteQuery : Combinator<MySqlConnection, MySqlDataReader>
+    {
+        [Editor("Bonsai.Design.RichTextEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        [Description("Specifies the full SQL query to run against the MySQL connection.")]
+        public string QueryString { get; set; }
+
+        public override IObservable<MySqlDataReader> Process(IObservable<MySqlConnection> source)
+        {
+            return source.SelectMany(async (connection, cancellationToken) =>
+            {
+                using var command = new MySqlCommand(QueryString, connection);
+                var reader = await command.ExecuteReaderAsync(cancellationToken);
+                return reader;
+            });
+        }
+    }
+}
