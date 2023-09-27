f1709f317bf16ed2804475c0845dadf925139d31
glopesdev <glopesdev@users.noreply.github.com>
2023-09-27 12:54:59 +0100

    Add support for sampling available system memory

---
+++ src/Aeon.Acquisition/AvailableMemory.cs
@@ -0,0 +1,21 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Aeon.Acquisition
+{
+    [Combinator]
+    [Description("Samples a performance counter reporting the available system memory, in megabytes.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class AvailableMemory
+    {
+        public IObservable<float> Process<TSource>(IObservable<TSource> source)
+        {
+            return Observable.Using(
+                () => new System.Diagnostics.PerformanceCounter("Memory", "Available MBytes"),
+                performance => source.Select(_ => performance.NextValue()));
+        }
+    }
+}
