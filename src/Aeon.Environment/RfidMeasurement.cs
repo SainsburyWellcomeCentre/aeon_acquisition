875113e8039938843c19c2b6594e87f83534b41b
glopesdev <glopesdev@users.noreply.github.com>
2023-10-23 17:40:50 +0100

    Add support for spatial parsing of rfid events

---
+++ src/Aeon.Environment/RfidMeasurement.cs
@@ -0,0 +1,21 @@
+﻿using OpenCV.Net;
+
+namespace Aeon.Environment
+{
+    public struct RfidMeasurement
+    {
+        public Point2f Location;
+        public ulong TagId;
+
+        public RfidMeasurement(Point2f location, ulong tagId)
+        {
+            Location = location;
+            TagId = tagId;
+        }
+
+        public override readonly string ToString()
+        {
+            return $"{nameof(RfidMeasurement)}(Location: {Location}, TagId: {TagId})";
+        }
+    }
+}
