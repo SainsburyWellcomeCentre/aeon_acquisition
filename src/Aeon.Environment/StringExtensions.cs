c57747b1181c65464a2ca741c217e9e15c6f2160
glopesdev <glopesdev@users.noreply.github.com>
2023-11-01 11:12:29 +0000

    Normalize line endings of inputs to label control

--- src/Aeon.Environment/StringExtensions.cs
+++ src/Aeon.Environment/StringExtensions.cs
@@ -1,10 +1,22 @@
-﻿namespace Aeon.Environment
+﻿using System.Text.RegularExpressions;
+
+namespace Aeon.Environment
 {
     static class StringExtensions
     {
         public static string AsNullIfEmpty(this string value)
         {
             return !string.IsNullOrEmpty(value) ? value : null;
+        }
+
+        public static string ReplaceLineEndings(this string value)
+        {
+            return ReplaceLineEndings(value, System.Environment.NewLine);
+        }
+
+        public static string ReplaceLineEndings(this string value, string newLine)
+        {
+            return Regex.Replace(value, @"\r\n|\n\r|\n|\r", newLine);
         }
     }
 }
