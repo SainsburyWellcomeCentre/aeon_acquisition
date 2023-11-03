ca125f887d7ed1fb17106a6004c23ce295ed659d
glopesdev <glopesdev@users.noreply.github.com>
2023-11-03 15:54:59 +0000

    Move colony queries to environment package

---
+++ src/Aeon.Environment/EnumerateColony.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Aeon.Database;
+using Bonsai;
+using MySqlConnector;
+
+namespace Aeon.Environment
+{
+    [Description("Enumerates all records in the colony table for each MySQL connection in the sequence.")]
+    public class EnumerateColony : Combinator<MySqlConnection, SubjectRecord>
+    {
+        internal static IObservable<SubjectRecord> Query(MySqlConnection connection)
+        {
+            return ObservableDatabase.Query<SubjectRecord>(
+                "SELECT `subject`,`sex`,`subject_birth_date`,`subject_description`,`lab_id`," +
+                "`responsible_fullname`,`gen_bg_id`,`strain_id`,`cage_number`,`available` " +
+                "FROM `aeon_subject`.`subject` NATURAL JOIN `aeon_subject`.`_subject_detail`;",
+                connection);
+        }
+
+        public override IObservable<SubjectRecord> Process(IObservable<MySqlConnection> source)
+        {
+            return source.SelectMany(Query);
+        }
+    }
+}
---
+++ src/Aeon.Environment/SubjectRecord.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeon.Environment
+{
+    public class SubjectRecord
+    {
+        [Column("subject")]
+        public string Id { get; set; }
+
+        [Column("sex")]
+        public SubjectSex Sex { get; set; }
+
+        [Column("subject_birth_date")]
+        public DateTime? BirthDate { get; set; }
+
+        [Column("subject_description")]
+        public string Description { get; set; }
+
+        [Column("lab_id")]
+        public string LabId { get; set; }
+
+        [Column("responsible_fullname")]
+        public string ResponsibleFullName { get; set; }
+
+        [Column("gen_bg_id")]
+        public int? GeneticBackgroundId { get; set; }
+
+        [Column("strain_id")]
+        public int? StrainId { get; set; }
+
+        [Column("cage_number")]
+        public string CageNumber { get; set; }
+
+        [Column("available")]
+        public bool Available { get; set; }
+
+        public override string ToString()
+        {
+            return $"({Id}, " +
+                $"sex: {Sex}, " +
+                $"dob: {BirthDate}, " +
+                $"description: {Description}, " +
+                $"lab_id: {LabId}, " +
+                $"available: {Available})";
+        }
+    }
+
+    public enum SubjectSex
+    {
+        Male = 'M',
+        Female = 'F',
+        Unspecified = 'U'
+    }
+}
