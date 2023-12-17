e4c7d29d755d5034c6185aa2ba61959357297f1b
glopesdev <glopesdev@users.noreply.github.com>
2023-12-17 20:52:02 +0000

    Add property grid view configuration properties

--- src/Aeon.Environment/ExperimentProperties.cs
+++ src/Aeon.Environment/ExperimentProperties.cs
@@ -1,10 +1,17 @@
 ﻿using Bonsai;
 using Bonsai.Expressions;
+using System.ComponentModel;
 
 namespace Aeon.Environment
 {
     [TypeVisualizer(typeof(ExternalizedPropertiesVisualizer))]
+    [Description("Provides a configurable visualizer for global workflow properties.")]
     public class ExperimentProperties : UnitBuilder
     {
+        [Description("Specifies whether the help text box is visible.")]
+        public bool HelpVisible { get; set; } = true;
+
+        [Description("Specifies whether the toolbar is visible.")]
+        public bool ToolbarVisible { get; set; } = true;
     }
 }
