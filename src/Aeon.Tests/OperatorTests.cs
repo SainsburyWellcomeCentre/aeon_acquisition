bf672da8821fbf90264aea33d42a9bc2ad5c710c
glopesdev <glopesdev@users.noreply.github.com>
2024-07-17 11:32:40 +0100

    Exclude driver dependent tests from linux run

--- src/Aeon.Tests/OperatorTests.cs
+++ src/Aeon.Tests/OperatorTests.cs
@@ -17,15 +17,20 @@ namespace Aeon.Tests
             var acquisition = typeof(GroupByTime).Assembly;
             var environment = typeof(EnvironmentState).Assembly;
             var foraging = typeof(WheelDisplacement).Assembly;
-            var video = typeof(VideoDataFrame).Assembly;
             var vision = typeof(DistanceFromPoint).Assembly;
             var sleap = typeof(FormatPose).Assembly;
             AssertWorkflow.CanBuildEmbeddedResources(acquisition);
             AssertWorkflow.CanBuildEmbeddedResources(environment);
             AssertWorkflow.CanBuildEmbeddedResources(foraging);
-            AssertWorkflow.CanBuildEmbeddedResources(video);
             AssertWorkflow.CanBuildEmbeddedResources(vision);
             AssertWorkflow.CanBuildEmbeddedResources(sleap);
+        }
+
+        [TestMethod, TestCategory("DriverDependent")]
+        public void Build_DriverDependentWorkflows()
+        {
+            var video = typeof(VideoDataFrame).Assembly;
+            AssertWorkflow.CanBuildEmbeddedResources(video);
         }
     }
 }
