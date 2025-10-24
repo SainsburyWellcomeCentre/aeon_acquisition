3e7daa7a528ca3a0fab5b6acb015ea17fb8ff395
glopesdev <g.lopes@neurogears.org>
2025-10-24 16:34:32 +0100

    Add operator for creating video data frame objects (#239)
    This allows more easily wrapping existing video metadata around
    post-processed videos for compatibility with logging operators.

---
+++ src/Aeon.Video/CreateVideoDataFrame.cs
@@ -0,0 +1,19 @@
+﻿using Bonsai;
+using OpenCV.Net;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Aeon.Video
+{
+    [Combinator]
+    [Description("Converts a sequence of image-metadata pairs into a sequence of VideoDataFrame objects.")]
+    public class CreateVideoDataFrame
+    {
+        public IObservable<VideoDataFrame> Process(IObservable<Tuple<IplImage, VideoChunkData>> source)
+        {
+            return source.Select(xs => new VideoDataFrame(xs.Item1, xs.Item2));
+        }
+    }
+}
