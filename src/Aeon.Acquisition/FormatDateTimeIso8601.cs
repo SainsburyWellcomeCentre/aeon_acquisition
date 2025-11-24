049afa5d5e1575fd395e3120842df19a9b49ca54
glopesdev <g.lopes@neurogears.org>
2025-11-24 15:11:49 +0000

    Add operator for date-time formatting into ISO8601
    The format string is identical to the round-trip "o" format except that
    the fractional part will be ommitted if it is equal to zero.

---
+++ src/Aeon.Acquisition/FormatDateTimeIso8601.cs
@@ -0,0 +1,24 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Aeon.Acquisition
+{
+    [Combinator]
+    [Description("Formats a date time into an ISO 8601 string with no illegal path characters.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class FormatDateTimeIso8601
+    {
+        public IObservable<string> Process(IObservable<DateTime> source)
+        {
+            return source.Select(value => value.ToString("yyyy-MM-ddTHHmmss.FFFFFFFK").Replace(":", string.Empty));
+        }
+
+        public IObservable<string> Process(IObservable<DateTimeOffset> source)
+        {
+            return source.Select(value => value.ToString("yyyy-MM-ddTHHmmss.FFFFFFFzzz").Replace(":", string.Empty));
+        }
+    }
+}
