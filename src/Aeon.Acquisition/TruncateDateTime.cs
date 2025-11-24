1a140cd870ed034a5f7b373ab3f9c68c1c6e9043
glopesdev <g.lopes@neurogears.org>
2025-11-24 15:13:09 +0000

    Add operator for truncating date-time objects
    This allows explicit truncation of fractional time information instead
    of having it implicit inside the date formatting operator.

---
+++ src/Aeon.Acquisition/TruncateDateTime.cs
@@ -0,0 +1,24 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Aeon.Acquisition
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Converts a sequence of date-time objects by truncating each value to whole seconds.")]
+    public class TruncateDateTime
+    {
+        public IObservable<DateTime> Process(IObservable<DateTime> source)
+        {
+            return source.Select(value => value.AddTicks(-(value.Ticks % TimeSpan.TicksPerSecond)));
+        }
+
+        public IObservable<DateTimeOffset> Process(IObservable<DateTimeOffset> source)
+        {
+            return source.Select(value => value.AddTicks(-(value.Ticks % TimeSpan.TicksPerSecond)));
+        }
+    }
+}
