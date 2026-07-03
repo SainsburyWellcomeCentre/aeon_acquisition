09ac5ae50744d0c60976aad6f2b26c9310acbeb4
ncguilbeault <ncguilbeault@gmail.com>
2026-07-03 14:57:27 +0100

    Add device monitor workflow

---
+++ src/Aeon.Ephys/ConvertToHarpTimestamp.cs
@@ -0,0 +1,19 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Aeon.Ephys;
+
+[Combinator]
+[Description("Converts a raw ONIX harp sync wholse second input data to fractional aligned seconds.")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class ConvertToHarpTimestamp
+{
+    public IObservable<double> Process(IObservable<uint> source)
+    {
+        return source.Select(value => (double)(value + 1));
+    }
+}
