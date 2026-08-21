fe4a3e9946d4c6eb4518122de7222504e91f323e
NeuroThom <t.ryan@neurogears.org>
2026-08-21 12:12:46 +0100

    Add byte conversion operator for binary logging
    Currently only needed for ephys.
    Flattens IMU quaternions and vectors, and 64-bit clock counters,
    into arrays that LogBinaryData's MatrixWriter can write.
    Arguably should live in Aeon.Ephys

---
+++ src/Aeon.Acquisition/ConvertToBytes.cs
@@ -0,0 +1,35 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Numerics;
+
+namespace Aeon.Acquisition
+{
+    [Combinator]
+    [Description("Convert various data representations to a serializable byte array.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class ConvertToBytes
+    {
+        public IObservable<float[]> Process(IObservable<Quaternion> source)
+        {
+            return source.Select(value => new[] { value.W, value.X, value.Y, value.Z });
+        }
+
+        public IObservable<float[]> Process(IObservable<Vector3> source)
+        {
+            return source.Select(value => new[] { value.X, value.Y, value.Z });
+        }
+
+        public IObservable<byte[]> Process(IObservable<ulong> source)
+        {
+            return source.Select(value => BitConverter.GetBytes(value));
+        }
+
+        public IObservable<byte[]> Process(IObservable<int> source)
+        {
+            return source.Select(value => BitConverter.GetBytes(value));
+        }
+    }
+}
